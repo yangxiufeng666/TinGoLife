8a0a869037496b861c98f3fe2b01b23e2d7445e7
ManongJu <469863752@qq.com>
2016-03-30 17:27:50 +0800

    commit splash

---
+++ TinGoLife/app/src/main/java/com/github/tingolife/activity/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.github.tingolife.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.tingolife.R;
+
+/**
+ * Created with com.github.tingolife.activity
+ * User:YangXiuFeng
+ * Date:2016/3/30
+ * Time:16:34
+ */
+public class SplashActivity extends BaseActivity{
+    private Handler handler;
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashActivity.this,TinGoMain.class);
+            SplashActivity.this.startActivity(intent);
+            finish();
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        handler = new Handler();
+        handler.postDelayed(runnable,1000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+    }
+}
